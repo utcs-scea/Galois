76d458d30f401636eb6c0646648c92e775836dc0
AdityaAtulTewari <adityaatewari@gmail.com>
2023-05-20 22:54:46 +0000

    Initial commit of instrumentation for Yineng

--- libgalois/include/galois/graphs/LS_LC_CSR_64_Graph.h
+++ libgalois/include/galois/graphs/LS_LC_CSR_64_Graph.h
@@ -35,6 +35,9 @@
 #include "galois/graphs/FileGraph.h"
 #include "galois/graphs/GraphHelpers.h"
 #include "galois/PODResizeableArray.h"
+#include "galois/Reduction.h"
+
+
 
 namespace galois::graphs {
 /**
@@ -69,6 +72,23 @@ class LS_LC_CSR_64_Graph :
   friend class LC_InOut_Graph;
 
 public:
+
+#if GRAPH_PROFILE
+  galois::Accumulator<uint64_t> local_seq_write_counts;
+  galois::Accumulator<uint64_t> local_seq_write_bytes;
+  galois::Accumulator<uint64_t> local_seq_read_counts;
+  galois::Accumulator<uint64_t> local_seq_read_bytes;
+  galois::Accumulator<uint64_t> local_seq_rmw_bytes;
+  galois::Accumulator<uint64_t> local_seq_rmw_counts;
+  galois::Accumulator<uint64_t> local_rnd_rmw_bytes;
+  galois::Accumulator<uint64_t> local_rnd_rmw_counts;
+  galois::Accumulator<uint64_t> local_rnd_write_counts;
+  galois::Accumulator<uint64_t> local_rnd_write_bytes;
+  galois::Accumulator<uint64_t> local_rnd_read_counts;
+  galois::Accumulator<uint64_t> local_rnd_read_bytes;
+#endif
+
+
   template <bool _has_id>
   struct with_id {
     typedef LS_LC_CSR_64_Graph type;
@@ -522,13 +542,30 @@ public:
     acquireNode(src, galois::MethodFlag::WRITE);
     auto orig_deg = getDegree(src);
     auto ee = edgeEnd.fetch_add(num_dst + orig_deg, std::memory_order_relaxed);
+
+#if GRAPH_PROFILE
+    this->local_rnd_rmw_counts += 1;
+    this->local_rnd_rmw_counts += 8;
+#endif
+
     auto edgeStart = ee;
     auto orig_itr   = edge_begin(src);
     auto orig_end   = edge_end(src);
     auto dst_end    = dst + num_dst;
 
     std::memcpy(&edgeDst[edgeStart], &edgeDst[*orig_itr], sizeof(EdgeDst::value_type) * orig_deg);
+
+#if GRAPH_PROFILE
+    this->local_seq_write_bytes += sizeof(EdgeDst::value_type) * orig_deg;
+    this->local_seq_write_counts += sizeof(EdgeDst::value_type) * orig_deg / 8;
+#endif
+
     std::memcpy(&edgeDst[edgeStart + orig_deg], dst, sizeof(EdgeDst::value_type) * num_dst);
+
+#if GRAPH_PROFILE
+    this->local_seq_write_bytes += sizeof(EdgeDst::value_type) * num_dst;
+    this->local_seq_write_counts += sizeof(EdgeDst::value_type) * num_dst / 8;
+#endif
 
     if(EdgeData::has_value && setEdgeVals)
     {
@@ -542,9 +579,37 @@ public:
       }
     }
 
+#if GRAPH_PROFILE
+    //Atomic for dense object space
+    this->local_rnd_rmw_counts += 1;
+    this->local_rnd_rmw_counts += 8;
+
+    //Copy data stuff
+    this->local_seq_write_bytes   += sizeof(EdgeTy) * num_dst;
+    this->local_seq_write_counts  += sizeof(EdgeTy) * num_dst / 8;
+
+    //hashmap modification
+    this->local_rnd_read_counts   += 1 * num_dst;
+    this->local_rnd_read_bytes    += 8 * num_dst;
+    this->local_rnd_write_counts  += 1 * num_dst;
+    this->local_rnd_write_bytes   += 8 * num_dst;
+#endif
+
+
     edgeIndData[src].first = edgeStart;
     edgeIndData[src].second = edgeStart + num_dst + orig_deg;
+
+#if GRAPH_PROFILE
+    this->local_rnd_write_counts  += 1;
+    this->local_rnd_write_bytes   += 8;
+#endif
+
     numEdges.fetch_add(num_dst, std::memory_order_relaxed);
+#if GRAPH_PROFILE
+    this->local_rnd_rmw_counts += 1;
+    this->local_rnd_rmw_counts += 8;
+#endif
+
   }
 
   void addEdgeSort(const uint64_t src, const uint64_t dst)
@@ -906,6 +971,7 @@ public:
       edgeData.allocateInterleaved(numEdges);
       this->outOfLineAllocateInterleaved(numNodes);
     }
+    //TODO How do we want to do memory?
   }
 
   void allocateFrom(uint64_t nNodes, uint64_t nEdges) {
@@ -925,6 +991,7 @@ public:
       edgeData.allocateInterleaved(numEdges);
       this->outOfLineAllocateInterleaved(numNodes);
     }
+    //TODO How do we want to do memory?
   }
 
   void destroyAndAllocateFrom(uint64_t nNodes, uint64_t nEdges) {
@@ -961,6 +1028,10 @@ public:
           this->outOfLineConstructAt(x);
         },
         galois::no_stats(), galois::loopname("CONSTRUCT_NODES"));
+#endif
+#if GRAPH_PROFILE
+    this->local_rnd_write_counts += sizeof(NodeInfo) * numNodes / 8;
+    this->local_rnd_write_bytes  += sizeof(NodeInfo) * numNodes;
 #endif
   }
 
