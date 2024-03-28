45c99dd18ea7d8ed2bbb27e2249b3098c214eb71
Hochan Lee <hochan@utexas.edu>
2024-03-28 12:03:00 -0500

    Fix lfs file tracking

--- inputs/tester/tester.el
+++ inputs/tester/tester.el
@@ -1,12 +1,3 @@
-0 1
-1 2
-1 0
-2 3
-2 1
-3 4
-3 2
-4 5
-4 3
-5 6
-5 4
-6 5
+version https://git-lfs.github.com/spec/v1
+oid sha256:f46e73a8c721cef981bb7ea0595da475203f674fb1da841d2d9cfc3900e7dd11
+size 48
