24b8db23c87970c5b28bc04152346c07ec9c9c41
Meyer Zinn <6132034+meyerzinn@users.noreply.github.com>
2024-04-03 17:15:45 -0500

    fix bug in LSCSR class visibility (#25)

--- libgalois/include/galois/graphs/LS_LC_CSR_Graph.h
+++ libgalois/include/galois/graphs/LS_LC_CSR_Graph.h
@@ -58,6 +58,9 @@ public:
 
   using EdgeHandle = std::pair<VertexTopologyID, VertexTopologyID>;
 
+  class EdgeIterator;
+  using EdgeRange = boost::iterator_range<EdgeIterator>;
+
 private:
   using SpinLock = galois::substrate::PaddedLock<concurrent>;
   static constexpr bool HasVertexData = !std::is_same_v<VertexData, void>;
@@ -76,9 +79,6 @@ private:
   // forward-declarations of internal structs
   struct VertexMetadata;
   struct EdgeMetadata;
-
-  class EdgeIterator;
-  using EdgeRange = boost::iterator_range<EdgeIterator>;
 
   VertexDataStore m_vertex_data;
   std::vector<VertexMetadata> m_vertices;
@@ -466,6 +466,7 @@ private:
 
   static_assert(sizeof(EdgeMetadata) <= sizeof(uint64_t));
 
+public:
   class EdgeIterator
       : public boost::iterator_facade<EdgeIterator, EdgeHandle,
                                       boost::bidirectional_traversal_tag,
